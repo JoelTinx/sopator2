a6da80d51707f8ba70939f55d331d58e933f5491
Joel Tinoco <wizard.joel@gmail.com>
2021-11-25 23:21:32 -0500

    feat: improvements in menu

--- src/components/menu/menu.component.jsx
+++ src/components/menu/menu.component.jsx
@@ -7,21 +7,30 @@ const Menu = () => {
   return (
     <nav>
       <MenuContainer>
-        <CustomNavLink to="/">
-          Inicio
-        </CustomNavLink>
-        <CustomNavLink to="/advices">
-          Consejos
-        </CustomNavLink>
-        <CustomNavLink to="/about">
-          Acerca de
-        </CustomNavLink>
+        <LogoContainer>
+          <NavLink to="/">
+            Sopator
+          </NavLink>
+        </LogoContainer>
+        <OptionsContainer>
+          <CustomNavLink to="/">
+            Inicio
+          </CustomNavLink>
+          <CustomNavLink to="/advices">
+            Consejos
+          </CustomNavLink>
+          <CustomNavLink to="/about">
+            Acerca de
+          </CustomNavLink>
+        </OptionsContainer>
       </MenuContainer>
     </nav>
   )
 }
 
 const MenuContainer = ({ children }) => <div className="menu-container">{ children }</div>
+const LogoContainer = ({ children }) => <div className="logo-container">{ children }</div>
+const OptionsContainer = ({ children }) => <div className="options-container">{ children }</div>
 
 const CustomNavLink = ({ children, ...props }) => (
   <NavLink {...props} className={({isActive}) => isActive ? 'menu-item menu-item--active' : 'menu-item'}>
